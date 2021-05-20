2836fea9cf2f194237ac523fdb3fe36de776cbc1
RacMcT <raccmct@gmail.com>
2021-05-20 12:28:42 -0500

    found bug

--- form.js
+++ form.js
@@ -2,13 +2,12 @@
 // Your registration is complete.
 // Your temporary password is: Eric\*1934");
 
-
-
-let firstName = document.getElementById("fname").value;
-let lastName = document.getElementById("lname").value;
-let birth = document.getElementById("birth").value;
+// bug- need to push the form value into the alert in a way the DOM recognizes
 
 function submitAlert (){
+  let firstName = document.getElementById("fname").value;
+  let lastName = document.getElementById("lname").value;
+  let birth = document.getElementById("birth").value;
   alert("Welcome " + firstName + " "+ lastName + "Your registration is complete\n Your temporary password is: " + fname + "*" + birth);
   console.log(firstName, lastName, birth)
 }
